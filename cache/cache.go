55fda687e5b34c8100b74530d948ce527782b7e3
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2024-07-04 19:20:57 +0400

    feat(cache): add continuous cache deletion and test functionality

--- cache/cache.go
+++ cache/cache.go
@@ -11,10 +11,12 @@ type cacheEntry struct {
 	createdAt time.Time
 }
 
-func NewCache() Cache {
-	return Cache{
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
 		cache: make(map[string]cacheEntry),
 	}
+	go c.DeleteContinuously(interval)
+	return c
 }
 
 func (c *Cache) Add(key string, val []byte) {
@@ -28,4 +30,21 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 	retrievedCache, ok := c.cache[key]
 
 	return retrievedCache.val, ok
+}
+
+func (c *Cache) DeleteContinuously(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+
+	for range ticker.C {
+		c.Delete(interval)
+	}
+}
+
+func (c *Cache) Delete(t time.Duration) {
+	threshold := time.Now().UTC().Add(-t)
+	for k, v := range c.cache {
+		if v.createdAt.Before(threshold) {
+			delete(c.cache, k)
+		}
+	}
 }
--- cache/cache_test.go
+++ cache/cache_test.go
@@ -1,9 +1,12 @@
 package cache
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestCreateNilCache(t *testing.T) {
-	cache := NewCache()
+	cache := NewCache(time.Millisecond)
 
 	if cache.cache == nil {
 		t.Error("cache is nil")
@@ -11,16 +14,53 @@ func TestCreateNilCache(t *testing.T) {
 }
 
 func TestAddGetCache(t *testing.T) {
-	cache := NewCache()
+	cache := NewCache(time.Millisecond)
 
-	cache.Add("key1", []byte("val1"))
-	actual, ok := cache.Get("key1")
-
-	if !ok {
-		t.Error("key1 not found")
+	cases := []struct {
+		inputKey string
+		inputVal []byte
+	}{
+		{
+			inputKey: "key1",
+			inputVal: []byte("val1"),
+		},
+		{
+			inputKey: "key2",
+			inputVal: []byte("val2"),
+		},
+		{
+			inputKey: "key3",
+			inputVal: []byte("val3"),
+		},
 	}
 
-	if string(actual) != "val1" {
-		t.Error("value does not match")
+	for _, c := range cases {
+		cache.Add(c.inputKey, c.inputVal)
+		actual, ok := cache.Get(c.inputKey)
+
+		if !ok {
+			t.Errorf("%s not found", c.inputKey)
+		}
+
+		if string(actual) != string(c.inputVal) {
+			t.Errorf("%s does not match %s", string(actual), string(c.inputVal))
+		}
+		continue
+	}
+}
+
+func TestDelete(t *testing.T) {
+	interval := time.Millisecond * 10
+	cache := NewCache(interval)
+
+	keyOne := "key1"
+	cache.Add(keyOne, []byte("val"))
+
+	time.Sleep(interval + time.Millisecond)
+
+	_, ok := cache.Get(keyOne)
+
+	if ok {
+		t.Errorf("%s should have been deleted", keyOne)
 	}
 }
