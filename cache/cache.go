6be9afdcfa45a94a7ec9fde51fb154a02c50f565
Abhiroop Santra <abhiroop.santra@gmail.com>
2024-07-04 17:09:28 +0400

    feat: Add cache implementation and tests

---
+++ cache/cache.go
@@ -0,0 +1,31 @@
+package cache
+
+import "time"
+
+type Cache struct {
+	cache map[string]cacheEntry
+}
+
+type cacheEntry struct {
+	val       []byte
+	createdAt time.Time
+}
+
+func NewCache() Cache {
+	return Cache{
+		cache: make(map[string]cacheEntry),
+	}
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.cache[key] = cacheEntry{
+		val:       val,
+		createdAt: time.Now().UTC(),
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	retrievedCache, ok := c.cache[key]
+
+	return retrievedCache.val, ok
+}
---
+++ cache/cache_test.go
@@ -0,0 +1,26 @@
+package cache
+
+import "testing"
+
+func TestCreateNilCache(t *testing.T) {
+	cache := NewCache()
+
+	if cache.cache == nil {
+		t.Error("cache is nil")
+	}
+}
+
+func TestAddGetCache(t *testing.T) {
+	cache := NewCache()
+
+	cache.Add("key1", []byte("val1"))
+	actual, ok := cache.Get("key1")
+
+	if !ok {
+		t.Error("key1 not found")
+	}
+
+	if string(actual) != "val1" {
+		t.Error("value does not match")
+	}
+}
